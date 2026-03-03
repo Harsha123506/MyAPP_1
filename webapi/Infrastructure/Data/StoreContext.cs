c5caa071808e865c40fc244e5f95c5a3d37a3fc2
harsha yerraguntla <hyerraguntla@evoketechnologies.com>
2026-03-03 16:53:57 +0530

    L2323_21

---
+++ webapi/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Data;
+
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class StoreContext: DbContext
+
+{
+	public StoreContext(DbContextOptions options) : base(options)
+	{
+
+	}
+
+	DbSet<Product> products { get; set; };
+}
